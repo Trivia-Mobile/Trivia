5f94c36b29cb62aa1ad20b0c7aa48c91bcfd7274
JeffreyLum <JeffreyLum@users.noreply.github.com>
2021-04-24 15:09:51 -0700

    Implement a bottom tab navigator
    Implement a bottom tab navigator for the coding categories screen and settings screen and merged it with the stack navigator

--- trivia/App.js
+++ trivia/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, TabBarIOS, Text, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
 
@@ -15,10 +17,10 @@ const AppButton = ({ onPress, title }) => (
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={ {width:"50%"} }>
-      <AppButton onPress={() => navigation.navigate('Coding Categories')} title="Play" />
+      <View style={{ width: "50%" }}>
+        <AppButton onPress={() => navigation.navigate('Coding Categories')} title="Play" />
       </View>
-      
+
     </View>
   );
 }
@@ -26,12 +28,12 @@ function HomeScreen({ navigation }) {
 function CodingCategoriesScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={ {width:"70%"} }>
-         <AppButton onPress={() => navigation.navigate('Coding Questions')} title="Quickplay" />
-      
+      <View style={{ width: "70%" }}>
+        <AppButton onPress={() => navigation.navigate('Coding Questions')} title="Quickplay" />
+
       </View>
-     
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+
+      <Button title="Go back" onPress={() => navigation.navigate('Home')} />
     </View>
   );
 }
@@ -39,11 +41,11 @@ function CodingCategoriesScreen({ navigation }) {
 function CodingQuestionScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      
-      <Button
+
+      {/* <Button
         title="Go to Settings"
         onPress={() => navigation.navigate('Settings')}
-      />
+      /> */}
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
@@ -52,22 +54,49 @@ function CodingQuestionScreen({ navigation }) {
 function SettingsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Text>Settings</Text>
+      {/* <Button title="Go back" onPress={() => navigation.goBack()} /> */}
     </View>
   );
 }
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Coding Questions" component={CodingQuestionScreen} />
-      <Stack.Screen name="Coding Categories" component={CodingCategoriesScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Coding Categories" component={MyBottomTabNavigator}
+        options={{ headerShown: false }} />
     </Stack.Navigator>
   );
+}
+function MyBottomTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Coding Categories') {
+            iconName = 'list-circle-outline';
+          } else if (route.name === 'Settings') {
+            iconName = 'settings-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Coding Categories" component={CodingCategoriesScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  )
 }
 
 export default function App() {
